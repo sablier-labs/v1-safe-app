76e22f9e3b860b2c87a503ddf81041316621673e
Paul Razvan Berg <hello@paulrberg.com>
2020-05-29 14:49:29 +0300

    refactor: nuke service worker out of orbit

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyles from "./global";
 
-import * as serviceWorker from "./serviceWorker";
-
 ReactDOM.render(
   <>
     <GlobalStyles />
@@ -13,8 +11,3 @@ ReactDOM.render(
   </>,
   document.getElementById("root"),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
