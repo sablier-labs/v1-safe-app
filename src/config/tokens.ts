4b8e7f179e23cb1ec3a74ffb996f580c1fdb15d9
Tom French <tom@tomfren.ch>
2020-06-09 22:57:37 +0100

    feat: add list of mainnet tokens

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -24,7 +24,20 @@ const tokens: TokenMap = {
   mainnet: {
     ETH: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
     DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    BAT: "0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
+    cDAI: "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643",
+    CHAI: "0x06AF07097C9Eeb7fD685c692751D5C66dB49c215",
+    cUSDC: "0x39AA39c021dfbaE8faC545936693aC917d5E7563",
+    KNC: "0xdd974D5C2e2928deA5F71b9825b8b646686BD200",
+    LINK: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+    MKR: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",
+    REP: "0x1985365e9f78359a9B6AD760e32412f4a445E862",
+    SNX: "0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F",
+    sUSD: "0x57Ab1E02fEE23774580C119740129eAC7081e9D3",
+    TUSD: "0x0000000000085d4780B73119b644AE5ecd22b376",
+    USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    WBTC: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
   },
   rinkeby: {
     ETH: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
@@ -39,14 +52,7 @@ export const getTokenList = (network: Networks): TokenItem[] => {
     throw Error(`No token configuration for ${network}`);
   }
 
-  return [
-    {
-      address: tokensByNetwork.DAI,
-      decimals: 18,
-      iconUrl: daiIcon,
-      id: "DAI",
-      label: "Dai",
-    },
+  const tokenList: TokenItem[] = [
     {
       address: tokensByNetwork.ETH,
       decimals: 18,
@@ -55,13 +61,105 @@ export const getTokenList = (network: Networks): TokenItem[] => {
       label: "ETH",
     },
     {
+      address: tokensByNetwork.DAI,
+      decimals: 18,
+      iconUrl: daiIcon,
+      id: "DAI",
+      label: "Dai",
+    },
+    {
+      address: tokensByNetwork.USDC,
+      decimals: 6,
+      iconUrl: wethIcon,
+      id: "USDC",
+      label: "USDC",
+    },
+    {
+      address: tokensByNetwork.cDAI,
+      decimals: 8,
+      iconUrl: daiIcon,
+      id: "DAI",
+      label: "Dai",
+    },
+    {
+      address: tokensByNetwork.cUSDC,
+      decimals: 8,
+      iconUrl: wethIcon,
+      id: "cUSDC",
+      label: "cUSDC",
+    },
+    {
+      address: tokensByNetwork.CHAI,
+      decimals: 18,
+      iconUrl: daiIcon,
+      id: "CHAI",
+      label: "CHAI",
+    },
+    {
+      address: tokensByNetwork.sUSD,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "sUSD",
+      label: "sUSD",
+    },
+    {
+      address: tokensByNetwork.TUSD,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "TUSD",
+      label: "TUSD",
+    },
+    {
+      address: tokensByNetwork.BAT,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "BAT",
+      label: "BAT",
+    },
+    {
+      address: tokensByNetwork.MKR,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "MKR",
+      label: "MKR",
+    },
+    {
+      address: tokensByNetwork.LINK,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "LINK",
+      label: "LINK",
+    },
+    {
+      address: tokensByNetwork.KNC,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "KNC",
+      label: "KNC",
+    },
+    {
+      address: tokensByNetwork.REP,
+      decimals: 18,
+      iconUrl: wethIcon,
+      id: "REP",
+      label: "REP",
+    },
+    {
+      address: tokensByNetwork.WBTC,
+      decimals: 8,
+      iconUrl: wethIcon,
+      id: "WBTC",
+      label: "WBTC",
+    },
+    {
       address: tokensByNetwork.WETH,
       decimals: 18,
       iconUrl: wethIcon,
       id: "WETH",
-      label: "Weth",
+      label: "WETH",
     },
   ];
+  return tokenList.filter(token => token.address !== undefined);
 };
 
 export default tokens;
