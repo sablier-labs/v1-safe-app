c06813dd7b6272a247b492ab1db72d7062dbb1be
Paul Razvan Berg <hello@paulrberg.com>
2021-04-22 10:48:20 +0300

    fix: bring back ComponentNameToClassKey type override

--- src/types/overrides-mui.d.ts
+++ src/types/overrides-mui.d.ts
@@ -3,3 +3,8 @@ import { MuiPickersOverrides } from "@material-ui/pickers/typings/overrides";
 type overridesNameToClassKey = {
   [P in keyof MuiPickersOverrides]: keyof MuiPickersOverrides[P];
 };
+
+declare module "@material-ui/core/styles/overrides" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ComponentNameToClassKey extends overridesNameToClassKey {}
+}
