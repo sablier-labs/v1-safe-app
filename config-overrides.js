bc72f3f373fddaefbd2c6387ddca0697034f01e8
Paul Razvan Berg <hello@paulrberg.com>
2021-04-22 10:38:19 +0300

    style: fix lint issues
    chore: create new tsconfig.dev.json for development ts files
    refactor: switch to yaml for eslint config file

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,24 @@
 module.exports = {
-  webpack: function (config, env) {
+  webpack: (config, _env) => {
     return config;
   },
-  jest: function (config) {
+  jest: config => {
     return config;
   },
-  devServer: function (configFunction) {
-    return function (proxy, allowedHost) {
+  devServer(configFunction) {
+    return (proxy, allowedHost) => {
       const config = configFunction(proxy, allowedHost);
 
       config.headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers":
-          "X-Requested-With, content-type, Authorization",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
       };
 
       return config;
     };
   },
-  paths: function (paths, env) {
+  paths(paths, _env) {
     return paths;
   },
 };
--- src/types/overrides-mui.d.ts
+++ src/types/overrides-mui.d.ts
@@ -3,7 +3,3 @@ import { MuiPickersOverrides } from "@material-ui/pickers/typings/overrides";
 type overridesNameToClassKey = {
   [P in keyof MuiPickersOverrides]: keyof MuiPickersOverrides[P];
 };
-
-declare module "@material-ui/core/styles/overrides" {
-  export interface ComponentNameToClassKey extends overridesNameToClassKey {}
-}
